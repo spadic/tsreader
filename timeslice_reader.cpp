570e74d46f60b4736a45de3234551813d292ffd0
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-07-02 16:00:30 +0200

    clean up timeslice_reader.cpp

--- timeslice_reader.cpp
+++ timeslice_reader.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-//#include <stdexcept>
 #include "TimesliceInputArchive.hpp"
 #include "TimesliceDebugger.hpp"
 
@@ -7,36 +6,37 @@ TimesliceDebugger d;
 
 void read_timeslice(const fles::Timeslice& ts)
 {
-    std::cout << d.dump_timeslice(ts) << std::endl;
+    std::cout << d.dump_timeslice(ts);
     for (auto c = 0; c < ts.num_components(); c++) {
         for (auto m = 0; m < ts.num_microslices(c); m++) {
             auto& desc = ts.descriptor(c, m);
             auto p = ts.content(c, m);
-            std::cout << d.dump_microslice(desc, p) << std::endl;
         }
     }
 }
 
-int main_(int argc, char* argv[])
+void read_timeslice_archive(const std::string& filename)
 {
-    fles::TimesliceInputArchive ar(argv[1]);
+    fles::TimesliceInputArchive ar(filename);
     while (auto p = ar.get()) {
         read_timeslice(*p);
     }
-    return 0;
 }
 
 int main(int argc, char* argv[])
 {
+    std::string filename;
     try {
-        return main_(argc, argv);
+        filename = argv[1];
+        read_timeslice_archive(filename);
+        return 0;
     }
     catch (std::logic_error& e) {
         std::cout << "Please specify path to .tsa file." << std::endl;
         return 1;
     }
     catch (boost::archive::archive_exception& e) {
-        std::cout << "Could not read \"" << argv[1] <<
+        std::cout << "Could not read \"" << filename <<
                      "\" as .tsa file." << std::endl;
         return 1;
     }
