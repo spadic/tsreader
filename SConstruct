47e97de66ec0255c27cd23ca9db25d94efcf8604
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-07-02 15:41:58 +0200

    use TimesliceDebugger, enhance SConstruct, handle errors

--- SConstruct
+++ SConstruct
@@ -2,18 +2,31 @@
 
 import os
 
-env = Environment(CXX='clang++', CCFLAGS=['-std=c++11'])
-env.Append(ENV={'TERM': os.environ['TERM']}) # clang color output
+use_clang = True
 
+# basic compiler settings
+env = Environment(CCFLAGS=['-std=c++11'])
+if use_clang:
+    env.Replace(CXX='clang++')
+    env.Append(ENV={'TERM': os.environ['TERM']}) # for color output
+
+# set path to flesnet sources
 try:
-    env.Append(FLES_DIR=os.environ['FLES_DIR'],
-               CPPPATH=['$FLES_DIR/lib/fles_ipc'])
+    env.Replace(FLES_DIR=os.environ['FLES_DIR'])
 except KeyError:
     raise SystemExit("Please set the FLES_DIR environment variable "
                      "to the root of the flesnet source tree.")
 
+# build fles_ipc library
+env.Append(CPPPATH=['$FLES_DIR/lib/fles_ipc'])
 fles_ipc = env.Library(env.Glob('$FLES_DIR/lib/fles_ipc/*.cpp'))
 
-env.Program('tsreader',
-            source=['timeslice_reader.cpp'],
-            LIBS=[fles_ipc, 'boost_serialization'])
+# build timeslice debugger
+env.Append(CPPPATH=['$FLES_DIR/app/tsclient'])
+ts_debugger = env.Library('$FLES_DIR/app/tsclient/TimesliceDebugger.cpp')
+
+#---------------------------------------------------------------------
+
+# build application
+env.Program('tsreader', ['timeslice_reader.cpp'],
+            LIBS=[fles_ipc, ts_debugger, 'boost_serialization'])
--- timeslice_reader.cpp
+++ timeslice_reader.cpp
@@ -1,22 +1,43 @@
 #include <iostream>
-#include <stdexcept>
+//#include <stdexcept>
 #include "TimesliceInputArchive.hpp"
+#include "TimesliceDebugger.hpp"
+
+TimesliceDebugger d;
 
 void read_timeslice(const fles::Timeslice& ts)
 {
+    std::cout << d.dump_timeslice(ts) << std::endl;
     for (auto c = 0; c < ts.num_components(); c++) {
-        std::cout << "component " << c << std::endl;
         for (auto m = 0; m < ts.num_microslices(c); m++) {
-            //auto& ms_desc = ts.descriptor(c, m);
-            //auto p = ts.content(c, m);
-            std::cout << "  microslice " << m << std::endl;
+            auto& desc = ts.descriptor(c, m);
+            auto p = ts.content(c, m);
+            std::cout << d.dump_microslice(desc, p) << std::endl;
         }
     }
 }
 
+int main_(int argc, char* argv[])
+{
+    fles::TimesliceInputArchive ar(argv[1]);
+    while (auto p = ar.get()) {
+        read_timeslice(*p);
+    }
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
-    std::cout << argv[1] << std::endl;
-    fles::TimesliceInputArchive ar(argv[1]);
-    return 0;
+    try {
+        return main_(argc, argv);
+    }
+    catch (std::logic_error& e) {
+        std::cout << "Please specify path to .tsa file." << std::endl;
+        return 1;
+    }
+    catch (boost::archive::archive_exception& e) {
+        std::cout << "Could not read \"" << argv[1] <<
+                     "\" as .tsa file." << std::endl;
+        return 1;
+    }
 }
