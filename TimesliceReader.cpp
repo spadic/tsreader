55f2eddebd859af41b1b3b04f03bade3132324b4
Michael Krieger <michael.krieger@ziti.uni-heidelberg.de>
2014-07-07 16:23:32 +0200

    added real implementation of TimesliceReader

--- TimesliceReader.cpp
+++ TimesliceReader.cpp
@@ -10,11 +10,16 @@ void TimesliceReader::add_timeslice(const fles::Timeslice& ts)
         for (size_t m {0}; m < ts.num_microslices(c); m++) {
             auto& desc = ts.descriptor(c, m);
             auto p = ts.content(c, m);
+            add_mc({p, desc.size});
         }
     }
 }
 
+void TimesliceReader::add_mc(const flib_dpb::MicrosliceContents& mc)
 {
+    for (auto& dtm : mc.dtms()) {
+        auto& reader = _readers[dtm.data[0]];
+        reader.add_buffer(dtm.data+1, dtm.size-1);
     }
 }
 
--- TimesliceReader.hpp
+++ TimesliceReader.hpp
@@ -3,17 +3,23 @@
 #ifndef SPADIC_TIMESLICE_READER
 #define SPADIC_TIMESLICE_READER
 
+#include <unordered_map>
+#include "Message.h"
 #include "Timeslice.hpp"
 #include "MicrosliceContents.hpp"
 
 namespace spadic {
 
+typedef std::unordered_map<uint16_t, spadic::MessageReader> reader_map;
 
 struct TimesliceReader
 {
     void add_timeslice(const fles::Timeslice& ts);
+    const reader_map& readers() { return _readers; };
 
 private:
+    void add_mc(const flib_dpb::MicrosliceContents& mc);
+    reader_map _readers; // one MessageReader per CBMnet source address
 };
 
 } // namespace
